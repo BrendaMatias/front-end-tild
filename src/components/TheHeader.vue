0bffb429199c80fb0beb5f29df83ad5b9d36b10f
Brenda <brendamatias.sobral@gmail.com>
2019-07-27 01:12:12 -0300

    Header e requisição dos posts

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,33 @@
+<template>
+    <header>
+        <div>
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+        <router-link to="/about">Contact</router-link>
+        <router-link to="/about">Category</router-link>
+        </div>
+    </header>
+</template>
+
+<script>
+
+export default {
+    name: 'TheHeader',
+}
+</script>
+
+<style lang="sass" scoped>
+header 
+    font-size: 12px
+    text-transform: uppercase
+    font-weight: 600
+    display: flex-inline
+    margin-top: 140px
+div
+    text-align: center
+
+a
+    color: black
+    padding: 0 20px
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,67 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-if="loaded">
+      <div v-for="post in posts" class="posts">
+        <div class="logo">
+          <img :src="post.image" />
+        </div>
+        <li>
+          <ul class="title">{{ post.title }}</ul>
+          <ul class="content">{{ post.content }}</ul>
+          <ul class="created_at">{{ post.created_at }}</ul>
+          <ul class="updated_at">{{ post.updated_at }}</ul>
+        </li>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import fetchData from "@/mixins/fetchData.js";
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
+  mixins: [fetchData],
+    data() {
+        return {
+            posts: null
+        }
+    },
+    methods: {
+    },
+    created() {
+        this.fetchData('posts');
+    }
 }
 </script>
+
+<style lang="sass" scoped>
+.home 
+  text-aling: center
+
+img 
+  width: 100%
+.home 
+  margin: 0 auto
+  width: 50%
+.home div
+  float: left
+.posts 
+  width: 250px 
+.title 
+  text-transform: uppercase
+  padding-bottom: 15px
+
+.content
+  padding-bottom: 15px
+
+.created_at
+  font-size: 12px
+  float: right
+.updated_at
+  font-size: 12px
+  float:left
+li 
+  margin: 40px
+</style>
+
